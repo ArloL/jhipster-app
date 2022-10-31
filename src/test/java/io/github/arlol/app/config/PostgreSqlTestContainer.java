373e717dbef5d1f6829fb38485d78e9d63fcb7cf
Arlo O'Keeffe <tiger@k5d.de>
2022-10-31 22:16:48 +0100

    change wait strategy to also wait for the container's port

--- src/test/java/io/github/arlol/app/config/PostgreSqlTestContainer.java
+++ src/test/java/io/github/arlol/app/config/PostgreSqlTestContainer.java
@@ -1,11 +1,15 @@
 package io.github.arlol.app.config;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Collections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
 
 public class PostgreSqlTestContainer implements SqlTestContainer {
 
@@ -24,11 +28,16 @@ public class PostgreSqlTestContainer implements SqlTestContainer {
 	@Override
     public void afterPropertiesSet() {
         if (null == postgreSQLContainer) {
+        	WaitAllStrategy waitStrategy = new WaitAllStrategy()
+        	    .withStartupTimeout(Duration.of(60, ChronoUnit.SECONDS))
+        	    .withStrategy(Wait.forListeningPort())
+        	    .withStrategy(Wait.forLogMessage(".*database system is ready to accept connections.*\\s", 2));
             postgreSQLContainer =
                 new PostgreSQLContainer<>("postgres:14.5")
                     .withDatabaseName("app")
                     .withTmpFs(Collections.singletonMap("/testtmpfs", "rw"))
                     .withLogConsumer(new Slf4jLogConsumer(log))
+                    .waitingFor(waitStrategy)
                     .withReuse(true);
         }
         if (!postgreSQLContainer.isRunning()) {
