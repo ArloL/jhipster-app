5ecc1fdafd42da59d77aebe4e8735ef49cd31d63
Arlo O'Keeffe <tiger@k5d.de>
2022-10-31 22:18:09 +0100

    tag IntegrationTest annotation

--- src/test/java/io/github/arlol/app/IntegrationTest.java
+++ src/test/java/io/github/arlol/app/IntegrationTest.java
@@ -6,6 +6,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
@@ -17,5 +18,6 @@ import org.springframework.test.annotation.DirtiesContext;
 @SpringBootTest(classes = { AppApp.class, AsyncSyncConfiguration.class })
 @EmbeddedSQL
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@Tag("integration")
 public @interface IntegrationTest {
 }
